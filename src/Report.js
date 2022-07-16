7da9609f3d21869bc24457682d2eef7943e53aa8
voidful <voidful.stack@gmail.com>
2022-07-16 20:16:58 +0800

    update report page

--- src/Report.js
+++ src/Report.js
@@ -6,28 +6,33 @@ class ReportPage extends React.Component {
         return (
             <div id="main">
                 <section className="post">
-                    <header>
-                        <h3>Report</h3>
-                    </header>
+                    <h2>Report</h2>
+                    <h3>Weekly Update</h3>
                     <div className="table-wrapper">
                         <table>
                             <thead>
                             <tr>
-                                <th>Week</th>
+                                <th>Title</th>
                                 <th>Link</th>
                             </tr>
                             </thead>
                             <tbody>
                             <tr>
-                                <td>Week 1</td>
+                                <td>Week 2</td>
                                 <td><a
-                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/gh-pages/report/%20JSALT%20Progress%20Report%20Week%201%20(Read%20only).pdf">PDF</a>
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/weekly%20report/JSALT-Progress-Report-Week-2.pdf">PDF</a>
                                 </td>
                             </tr>
                             <tr>
-                                <td>Week 2</td>
+                                <td>Week 1</td>
                                 <td><a
-                                    href="https://raw.githubusercontent.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/main/public/report/JSALT-Progress-Report-Week-2.pdf">PDF</a>
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/weekly%20report/JSALT%20Progress%20Report%20Week%201%20(Read%20only).pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Opening</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/opening/opening%20(v5).pdf">PDF</a>
                                 </td>
                             </tr>
                             </tbody>
@@ -35,7 +40,201 @@ class ReportPage extends React.Component {
                             </tfoot>
                         </table>
                     </div>
-
+                    <h3>June</h3>
+                    <div className="table-wrapper">
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Link</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>S3PRL</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(June)/(JSALT)%20S3PRL%20Toolkit.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Using SSL models for Multilingual ASR</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(June)/12062022_Using%20SSL%20models%20for%20Multilingual%20ASR.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Mel-Hubert</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(June)/6_12%20JSALT%20Team%20Meeting.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Cascaded CLIP</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(June)/CascadedCLIP_forJSALT.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Combine Speech&Text SSL</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(June)/Combine%20Speech_Text%20SSL%20-%20JSALT0611TeamMeeting.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Domain Generalization for Small Self-supervised Speech Processing Models</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(June)/Domain%20Generalization%20for%20Small%20Self-supervised%20Speech%20Processing%20Models.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Winning the Initialization Lottery</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(June)/HuBERT%20-%20Weight%20Initialization%20-%20June%2012%202022.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Prosody</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(June)/JASLT-Pretrained-Prosody-0612.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>How to better leverage pre-trained speech models: prompt & adapter</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(June)/JSALT%20onsite%20meeting%200612_%20prompt%20and%20adaptor.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>VISUALLY-ENHANCED SELF-SUPERVISED SPEECH MODELS</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(June)/JSALT_June_13_2022%20(Visual).pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Pretrained Models for Prosody: Track Plan</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(June)/intro-for-prosody.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Improving Generalization Capability of Pre-trained Model</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(June)/team%20meet%200612%20(robust)%20-%20basic%20idea.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            </tbody>
+                            <tfoot>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <h3>April</h3>
+                    <div className="table-wrapper">
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Link</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>Adapting Speech SSL to Text</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(April)/CombiningSSL.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Winning the Initialization Lottery</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(April)/HuBERT%20-%20Weight%20Initialization.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>SSL-Prosody</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(April)/JSALT%20Team%20meeting%20SSL-Prosody.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Using SSL models for Multilingual ASR</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(April)/Presentation%20JSALT%20SSL%202022%20-%20Multilingual.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Robustness of pre-trained speech models</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(April)/Robustness%20of%20pre-trained%20speech%20models.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Visually-Enhanced Self-Supervised Speech Models</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(April)/VG_JSALT_meeting_April_25.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Compressing Self-Supervised Models</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(April)/compression.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            </tbody>
+                            <tfoot>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <h3>March</h3>
+                    <div className="table-wrapper">
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Link</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>How to better leverage pre-trained speech models</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(March)/JSALT%20pre-meeting%200311%20prompt%20and%20adaptor.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Compressing Self-Supervised Models</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(March)/model-compression.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Self-Supervised Trained (SST) Models for Multilingual ASR</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(March)/multilingual-track-slides.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Pretrained Models for Prosody: Track Plan</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(March)/prosody-track-march-slides.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Improving Generalizability of Self-supervised Speech Models</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(March)/robustness.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Visually-Enhanced Self-Supervised Speech Models</td>
+                                <td><a
+                                    href="https://github.com/JSALT-2022-SSL/JSALT-2022-SSL.github.io/raw/main/public/report/Team%20meeting%20(March)/visually_enhanced.pdf">PDF</a>
+                                </td>
+                            </tr>
+                            </tbody>
+                            <tfoot>
+                            </tfoot>
+                        </table>
+                    </div>
                 </section>
             </div>
         )
